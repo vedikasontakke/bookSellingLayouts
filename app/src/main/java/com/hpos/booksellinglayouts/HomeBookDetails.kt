ed13ed5881e194f2c5382ddaf94cef29168e730e
Lenovo <vedikasontakke2003@gmail.com>
2021-02-26 15:09:38 -0300

    intial commit

---
+++ app/src/main/java/com/hpos/booksellinglayouts/HomeBookDetails.kt
@@ -0,0 +1,17 @@
+package com.hpos.booksellinglayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_home_book_details.*
+
+class HomeBookDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_book_details)
+
+       //seller_location.Paint.UNDERLINE_TEXT_FLAG)
+
+    }
+}
+
